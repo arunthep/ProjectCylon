b7632ac35834249149ecf167e858ae47aca5da93
kamol <kamolcu@gmail.com>
2013-11-25 09:42:07 +0700

    CLEAN UP *Page.py before generate on Windows platform

--- ProjectCylon/pageobjectdefinition/GenAllPageObject.bat
+++ ProjectCylon/pageobjectdefinition/GenAllPageObject.bat
@@ -1,4 +1,7 @@
 @echo off
+REM CLEAN UP *Page.py
+del /F /Q .\..\pageobject\*Page.py >NUL 2>&1
+
 dir /b .\*Page.csv
 for /f %%f in ('dir /b .\*Page.csv') do python GenPageObject.py --InputFile %%f --OutputDir .\..\pageobject
 echo #All Page Objects > .\..\pageobject\AllPages.py
