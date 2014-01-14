cbfb83e45fc08a75eb0c605a9378cfe617d8d97e
kamol <kamolcu@gmail.com>
2014-01-14 12:31:36 +0700

    Remove duplicate "import time"

--- ProjectCylon/framework/WorldContext.py
+++ ProjectCylon/framework/WorldContext.py
@@ -11,7 +11,7 @@ from selenium.common.exceptions        import TimeoutException
 from selenium.webdriver.support.ui     import WebDriverWait
 from selenium.webdriver.support        import expected_conditions as EC
 import selenium.webdriver.support.ui                              as ui
-import time
+
 
 class Singleton:
     """
