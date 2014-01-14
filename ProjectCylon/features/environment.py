f9745d1789b5069d9e79c68a14535ebbcfddca81
lek <leesupansa@gmail.com>
2014-01-14 13:55:31 +0700

    added tag ‘new_browser’

--- ProjectCylon/features/environment.py
+++ ProjectCylon/features/environment.py
@@ -5,7 +5,14 @@ World = WorldContext.Instance()
 def before_all(context):
 	import	framework.ContextObjects.User
 	import	framework.ContextObjects.LoadUser
+
+
+def before_tag(context, tag):
 	
+	if tag=='new_browser':
+		World.driver.quit()
+		World.driver = webdriver.Firefox()
+		World.driver.implicitly_wait(3)    
 #def after_all(context):
 #	World.driver.quit()
 
